03dbb38e03d2174438239dbb559ed8a03d318ffa
SUNGHO-DESKTOP\andyk <andykr1234@gmail.com>
2022-09-27 16:06:41 +0900

    파일에 데이터 저장하기

--- fileio_example/lib/fileApp.dart
+++ fileio_example/lib/fileApp.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FileApp extends StatefulWidget {
   const FileApp({Key? key}) : super(key: key);
@@ -12,17 +13,68 @@ class FileApp extends StatefulWidget {
 
 class _FileAppState extends State<FileApp> {
   int _count = 0;
+  List<String> itemList = List.empty(growable: true);
+  TextEditingController controller = TextEditingController();
+
+  Future<List<String>> readListFile() async {
+    List<String> itemList = List.empty(growable: true);
+    // var key = 'count';
+    var key = 'first';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    bool? firstCheck = pref.getBool(key);
+    var dir = await getApplicationDocumentsDirectory();
+
+    bool fileExist = await File('${dir.path}/fruit.txt').exists();
+    debugPrint('fileExist : ${fileExist.toString()}');
+    if (firstCheck == null || firstCheck == false || fileExist == false) {
+      pref.setBool(key, true);
+      var file =
+          await DefaultAssetBundle.of(context).loadString('repo/fruit.txt');
+      debugPrint(file);
+      File('${dir.path}/fruit.txt').writeAsStringSync(file);
+
+      var array = file.split('\n');
+      for (var item in array) {
+        debugPrint('item : $item');
+        itemList.add(item);
+      }
+    } else {
+      var file = await File('${dir.path}/fruit.txt').readAsString();
+      var array = file.split('\n');
+      debugPrint(array.toString());
+      for (var item in array) {
+        debugPrint(item);
+        itemList.add(item);
+      }
+    }
+
+    return itemList;
+  }
+
+  void writeFruit(String fruit) async {
+    var dir = await getApplicationDocumentsDirectory();
+    var file = await File('${dir.path}/fruit.txt').readAsString();
+    file = '$file\n$fruit';
+    File('${dir.path}/fruit.txt').writeAsStringSync(file);
+  }
+
+  void initData() async {
+    var result = await readListFile();
+    setState(() {
+      itemList.addAll(result);
+    });
+  }
 
   void writeCountFile(int count) async {
     var dir = await getApplicationDocumentsDirectory();
-    File(dir.path + '/count.txt').writeAsStringSync(count.toString());
+    File('${dir.path}/count.txt').writeAsStringSync(count.toString());
   }
 
   void readCountFile() async {
     try {
       var dir = await getApplicationDocumentsDirectory();
-      var file = await File(dir.path + '/count.txt').readAsString();
-      debugPrint(file);
+      var file = await File('${dir.path}/count.txt').readAsString();
+      debugPrint('file : $file');
       setState(() {
         _count = int.parse(file);
       });
@@ -34,7 +86,8 @@ class _FileAppState extends State<FileApp> {
   @override
   void initState() {
     super.initState();
-    readCountFile();
+    // readCountFile();
+    initData();
   }
 
   @override
@@ -43,22 +96,44 @@ class _FileAppState extends State<FileApp> {
       appBar: AppBar(
         title: const Text('File Example'),
       ),
-      body: Container(
-        child: Center(
-          child: Text(
-            '$_count',
-            style: const TextStyle(fontSize: 40),
-          ),
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          writeFruit(controller.value.text);
           setState(() {
-            _count++;
+            itemList.add(controller.value.text);
           });
-          writeCountFile(_count);
+          // setState(() {
+          //   _count++;
+          // });
+          // writeCountFile(_count);
         },
         child: const Icon(Icons.add),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.text,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Center(
+                          child: Text(
+                        itemList[index],
+                        style: TextStyle(fontSize: 30),
+                      )),
+                    );
+                  },
+                  itemCount: itemList.length,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
